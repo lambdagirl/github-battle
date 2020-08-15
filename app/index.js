4a7b6923a0e36d20bf34dbd057b5d245114672e1
Peizhi Zheng <pzheng16@asu.edu>
2020-08-14 17:31:13 -0700

    add css

--- app/index.js
+++ app/index.js
@@ -1,16 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Popular from "./components/Popular";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Popular from './components/Popular'
 
 class App extends React.Component {
   render() {
     return (
-      <div className="container">
+      <div className='container'>
         <Popular />
       </div>
-    );
+    )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
