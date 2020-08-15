310f32d2f363ad68807de5e0761eff88734e21f6
Peizhi Zheng <pzheng16@asu.edu>
2020-08-14 17:44:26 -0700

    navbar state

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React from 'react'
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selectedLanguage: 'All'
+    }
+
+    this.updateLanguage = this.updateLanguage.bind(this)
+  }
+  updateLanguage (selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    })
+  }
   render() {
-    const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
 
     return (
-      <ul className="flex-center">
+      <ul className='flex-center'>
         {languages.map((language) => (
           <li key={language}>
-            <button className="btn-clear nav-link">{language}</button>
+            <button
+              className='btn-clear nav-link'
+              style={language === this.state.selectedLanguage ? { color: 'rgb(187, 46, 31)' } : null}
+              onClick={() => this.updateLanguage(language)}>
+              {language}
+            </button>
           </li>
         ))}
       </ul>
-    );
+    )
   }
 }
