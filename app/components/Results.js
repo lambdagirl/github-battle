a5253a77363f9290d5fdace6ffdbda20f31ab31c
Peizhi Zheng <pzheng16@asu.edu>
2020-08-17 13:48:30 -0700

    result data

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,11 +1,50 @@
 import React from "react";
 import { battle } from "../utils/api";
 
-export default function Result(state) {
-  const { playerOne, playerTwo } = state;
-    battle([playerOne, playerTwo]).then((players) => {
-      console.log("data: ", players);
+function BattleReducer(state,action){
+    if (action.type === 'success'){
+        return ({
+            winner: action.winner ,
+            loser: action.loser,
+            error: null,
+            loading: false
+        })
+    } else if (action.type === 'error'){
+        return {
+            ...state,
+            error: action.message,
+            loading: false
+        } 
+    } else {
+        throw new Error(`That action type isn't supported`)
+    }
+}
+
+export default function Result(location) {
+    const { playerOne, playerTwo } = location
+    const [state, dispatch] = React.useReducer(BattleReducer, {
+        winner: null,
+        loser: null,
+        error: null,
+        loading: true
     })
+    React.useEffect(()=>{
+        battle([playerOne, playerTwo])
+        .then((players) => dispatch({type:'success', winner: players[0], loser: players[1]}))
+        .catch(({message}) => dispatch({type:'error', message}))
+    },[playerOne, playerTwo])
+
+    const {winner,loser,error,loading} = state
+
+    if (loading === true){
+        return <p>Loading</p>
+    }
+
+    if (error) {
+        return (
+            <p>{error}</p>
+        )
+    }
   return (
     <div>
       Results
